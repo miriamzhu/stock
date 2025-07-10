d0a6bc1b4dcf28ea4716b5b88af8274f225b93ab
Miriam Zhu <miriamzhu@Miriams-Macbook-Pro-2.local>
2025-07-09 21:35:56 -0400

    trained model

--- stock.py
+++ stock.py
@@ -1,11 +1,35 @@
 import yfinance as yf
 import matplotlib.pyplot as plt
+import pandas as pd
 
 sp500 = yf.Ticker("^GSPC")
 sp500 = sp500.history(period="max")
 sp500.plot.line(y="Close",use_index=True)
 
-plt.show()
-
 del sp500["Dividends"]
 del sp500["Stock Splits"]
+
+sp500["Tomorrow"] = sp500["Close"].shift(-1)
+
+sp500["Target"] = (sp500["Tomorrow"] > sp500["Close"]).astype(int)
+
+sp500 = sp500.loc["1990-01-01":].copy()
+
+from sklearn.ensemble import RandomForestClassifier
+
+model = RandomForestClassifier(n_estimators=100, min_samples_split=100, random_state=1)
+
+train = sp500.iloc[:-100]
+test = sp500.iloc[-100:] 
+
+predictors = ["Close", "Volume", "Open", "High", "Low"]
+model.fit(train[predictors], train["Target"])
+
+from sklearn.metrics import precision_score
+
+preds = model.predict(test[predictors])
+preds = pd.Series(preds, index=test.index)
+
+combined = pd.concat([test["Target"], preds], axis=1)
+combined.plot()
+plt.show()
