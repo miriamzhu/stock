f14bc5ba82c977af0e93885cf4e562223c2fc2fe
Miriam Zhu <miriamzhu@Miriams-Macbook-Pro-2.local>
2025-07-09 21:14:51 -0400

    import market data

--- stock.py
+++ stock.py
@@ -1,1 +1,11 @@
-print("Hello World")
+import yfinance as yf
+import matplotlib.pyplot as plt
+
+sp500 = yf.Ticker("^GSPC")
+sp500 = sp500.history(period="max")
+sp500.plot.line(y="Close",use_index=True)
+
+plt.show()
+
+del sp500["Dividends"]
+del sp500["Stock Splits"]
